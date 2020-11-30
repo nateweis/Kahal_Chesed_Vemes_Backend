a1540c73e9efbed2201cc6f9b539a9c1b6e44718
Naftali Weiss <Naftali@PROPUMP.local>
2020-11-30 15:31:43 -0500

    added bcrypt for password creation

--- models/user.js
+++ models/user.js
@@ -1,9 +1,11 @@
 const db = require('../db/db_connection')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 require('dotenv').config()
 const secret = process.env.SECRET
 
 const newUser = (req, res) => {
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
     db.none('INSERT INTO users (username, password, email, admin) VALUES (${username}, ${password}, ${email}, ${admin})', req.body)
     .then(()=>res.json({message:"Username Made"}))
     .catch(err=> res.json({err, message:"User not made"}))
