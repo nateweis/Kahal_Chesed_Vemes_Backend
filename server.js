1d3f676f83e17494be597e8bd084b89884155c46
Naftali Weiss <Naftali@PROPUMP.local>
2022-04-05 15:14:12 -0400

    added checkout stuff to backend

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 require('dotenv').config()
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const uuid = require("uuid/v4");
 
 const app = express();
 const port = process.env.PORT || 3001 ; 
@@ -26,5 +28,52 @@ app.use((req, res, next) => {
 // ////////////////////////////////////////
 const userContoller = require('./controllers/userRoutes')
 app.use('/users', userContoller)
+
+// Stripe Charge Route
+app.post("/stripeCheckout", async (req, res) => {
+  console.log("Request:", req.body);
+
+  let error;
+  let status;
+  try {
+    const { product, token } = req.body;
+
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id
+    });
+
+    const idempotency_key = uuid();
+    const charge = await stripe.charges.create(
+      {
+        amount: product.amount,
+        currency: "usd",
+        customer: customer.id,
+        receipt_email: token.email,
+        description: `${product.name}`,
+        shipping: {
+          name: token.card.name,
+          address: {
+            line1: token.card.address_line1,
+            line2: token.card.address_line2,
+            city: token.card.address_city,
+            country: token.card.address_country,
+            postal_code: token.card.address_zip
+          }
+        }
+      },
+      {
+        idempotency_key
+      }
+    );
+    console.log("Charge:", { charge });
+    status = "success";
+  } catch (error) {
+    console.error("Error:", error);
+    status = "failure";
+  }
+
+  res.json({ error, status });
+});
 
 app.listen(port, ()=>{console.log("Khala Chesed Vemes backend is running on port "+ port + (process.env.PORT? " :: env working" : " ") )})
